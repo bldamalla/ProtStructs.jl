6f34e2eab60f4df0ce9afb6aa3219abe6311008d
Bon Leif Amalla <bonleifamalla@gmail.com>
2022-03-07 18:06:14 +0900

    Define preliminary model for assignments
    * Test soon; declare some types at least, i guess
    * Overall, this looks fine. Just try to get it to work

--- src/Assignments.jl
+++ src/Assignments.jl
@@ -1,19 +1,48 @@
 ## Assignments.jl --- for secondary structure assignments/classifications
 
-"""
-    abstract type AssignmentScheme
+abstract type AbstractAssignmentLayer end
+abstract type AbstractAssignment <: Integer end
+abstract type AbstractCriterion{A<:AbstractAssignment} end
 
-Supertype of all secondary structure assignment schemes. Plans are to implement 
-DSSP, STRIDE, KAKSI, (three-state) HEO schemes.
-"""
-abstract type AssignmentScheme end
+const AA = AbstractAssignment
+const AC = AbstractCriterion
 
-struct DSSP <: AssignmentScheme end
-struct STRIDE <: AssignmentScheme end
-struct KAKSI <: AssignmentScheme end
-struct HEO <: AssignmentScheme end
+assignmenttype(::Type{<:AC{A}}) where A = A
 
-### TODO: declare enums for secondary structures assigned by each scheme
-### TODO: abstraction for creating and applying assignment criteria
-### TODO: calculation of criteria specifics (in respective files)
+### TODO: operations on subtypes of AbstractAssignment
+for op in (:+, :-, :|, :&)
+    @eval begin
+        function Base.$op(a1::A, a2::A) where A <: AA
+            (Int32(a1) + Int32(a2)) |> A
+        end
+    end
+end
+
+struct DiscreteCriterion{A} <: AC{A}
+    func::F where F <: Function
+    assignment::A
+    fallback::A
+end
+function (dc::DiscreteCriterion)(x...; kwargs...)
+    dc.func(x...; kwargs...) && return dc.assignment
+    return dc.fallback
+end
+
+struct CompoundCriterion{A} <: AC{A}
+    reduction::F where F <: Function
+    collection::NTuple{N,C{A}} where C<:AC
+end
+function (cc::CompoundCriterion)(x...; kwargs...)
+    (; reduction, collection) = cc
+    return mapreduce(reduction, collection) do criterion
+        criterion(x...; kwargs...)
+    end
+end
+
+## TODO: For now, stick with these two squash ops; build more i think
+
+function squash(reduction, criterion::C{A}...) where C<:AC
+    return CompoundCriterion{A}(reduction, criterion)
+end
+squash(criterion::C...) = squash(Base.:+, criterion...)
 
